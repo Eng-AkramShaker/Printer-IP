e29405b116b7d59cfe294d7c705989bb6434966e
Eng-AkramShaker <akramaboubakersh@gmail.com>
2023-07-19 10:30:05 +0200

    up

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Printer Demo By Serag Sakr',
       navigatorKey: appNavigatorKey,
       theme: ThemeData(
